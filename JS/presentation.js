98d5740bf630976db801e53d9383db31e75d7263
niles07 <159716080+niles07@users.noreply.github.com>
2024-10-31 23:27:07 +0300

    Update presentation.js

--- JS/presentation.js
+++ JS/presentation.js
@@ -2,4 +2,4 @@
 let path_to_file = null
 path_to_file = localStorage.path_to_file;
 const iframe_s = document.querySelector("iframe");
-iframe_s.setAttribute("href", path_to_file);
+iframe_s.setAttribute("src", path_to_file);
